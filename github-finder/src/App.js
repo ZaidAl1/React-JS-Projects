cd4030db6cfd7a58b5d4b8936e52c718d84ba9c3
ZaidAl1 <68479914+ZaidAl1@users.noreply.github.com>
2021-09-13 20:33:22 +0500

    Project GitHub-Finder

---
+++ github-finder/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Now i can see</h1>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
